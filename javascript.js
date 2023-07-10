19c06372a46ad88ca2efaa2204764eb54f439a98
Budzinskaite-Sediene <136670468+Budzinskaite-Sediene@users.noreply.github.com>
2023-07-10 16:10:10 +0300

    2 komitas

--- javascript.js
+++ javascript.js
@@ -1,16 +1,25 @@
-// 1. Pirma užduotis
+let aktoriausVardas = "Richard";
+let aktoriausPavarde = "Ghere";
 
-let x = "Richard";
-let y = "Ghere";
-
-if (x.length > y.length) {
-  console.log(y);
-} else {
-  console.log(vardas);
+if (aktoriausVardas.length > aktoriausPavarde.length) {
+  console.log(aktoriausPavarde);
 }
 
 let gimimoMetai = 1986;
 let sieMetai = 2023;
-let vardas = "Daiva";
-let pavarde = "Budzinskaitė-Šedienė";
+let vardasPavarde = "Daiva Budzinskaite-Sediene";
 let metuSkaicius = sieMetai - gimimoMetai;
+
+console.log(
+  "Aš esu " + vardasPavarde + " . Man yra " + metuSkaicius + " metai"
+);
+
+let paskutines3 =
+  aktoriausVardas[aktoriausVardas.length - 3] +
+  aktoriausVardas[aktoriausVardas.length - 2] +
+  aktoriausVardas[aktoriausVardas.length - 1] +
+  aktoriausPavarde[aktoriausPavarde.length - 3] +
+  aktoriausPavarde[aktoriausPavarde.length - 2] +
+  aktoriausPavarde[aktoriausPavarde.length - 1];
+
+console.log(paskutines3);
