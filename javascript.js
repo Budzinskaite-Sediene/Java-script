a61da617298c529cc06e2ee93ff12a19e5992170
Budzinskaite-Sediene <136670468+Budzinskaite-Sediene@users.noreply.github.com>
2023-07-10 14:44:32 +0300

    1 komitas

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// 1. Pirma užduotis
+
+let x = "Richard";
+let y = "Ghere";
+
+if (x.length > y.length) {
+  console.log(y);
+} else {
+  console.log(vardas);
+}
+
+let gimimoMetai = 1986;
+let sieMetai = 2023;
+let vardas = "Daiva";
+let pavarde = "Budzinskaitė-Šedienė";
+let metuSkaicius = sieMetai - gimimoMetai;
