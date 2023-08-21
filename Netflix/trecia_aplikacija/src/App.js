82a9dfc3d3a8738c12e15a89d390c17395d86978
Budzinskaite-Sediene <136670468+Budzinskaite-Sediene@users.noreply.github.com>
2023-08-21 08:43:21 +0300

    another commit

---
+++ Netflix/trecia_aplikacija/src/App.js
@@ -0,0 +1,13 @@
+import "./App.js";
+import Header from "./components/header/Header";
+
+function App() {
+  return;
+  {
+    <>
+      <Header />
+    </>;
+  }
+}
+
+export default App;
---
+++ Netflix/trecia_aplikacija/src/components/header/Header.js
@@ -0,0 +1,18 @@
+import "./Header.css";
+
+const Header = () => {
+  return;
+  {
+    <>
+      <header>
+        <ul>
+          <li>About</li>
+          <li>About</li>
+          <li>About</li>
+        </ul>
+      </header>
+    </>;
+  }
+};
+
+export default Header;
