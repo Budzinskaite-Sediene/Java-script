de9d71fc477c205eb52e1ef845cb284a0c259489
Budzinskaite-Sediene <136670468+Budzinskaite-Sediene@users.noreply.github.com>
2023-08-22 13:11:09 +0300

    hero commit

--- Netflix/trecia_aplikacija/src/components/header/Header.js
+++ Netflix/trecia_aplikacija/src/components/header/Header.js
@@ -1,18 +1,24 @@
-import "./Header.css";
+import logo from "./Netflix-Symbol.png";
 
 const Header = () => {
-  return;
-  {
-    <>
-      <header>
-        <ul>
-          <li>About</li>
-          <li>About</li>
-          <li>About</li>
-        </ul>
-      </header>
-    </>;
-  }
+  return (
+    <div className="header container d-flex justify-content-between align-items-center py-3">
+      <div className="logo">
+        <img src={logo} alt="netflix" className="n-logo" />
+      </div>
+      <div className="nav gap-3">
+        <div className="select">
+          <select className="form-select form-select-sm text-center bg-dark text-white">
+            <option className="bg-white" value="Lithuanian">
+              English
+            </option>
+            <option value="Lithuanian">Lithuanian</option>
+          </select>
+        </div>
+        <button className="btn btn-danger btn-sm">Sign in</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
